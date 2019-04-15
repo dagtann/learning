5fa5f9d086675473ebe0078c606efe7929726f27
Dag Tanneberg <dag.tanneberg@googlemail.com>
2019-04-15 07:05:41 +0200

    Prettify format of ds4business ch3 code

--- ds4Business/ch3.R
+++ ds4Business/ch3.R
@@ -1,8 +1,6 @@
 rm(list = ls())
 
-# Example: Attribute selection with entropy
-# Reproduce entropy charts ====================================================
-# Extract informative features
+# Example: Attribute selection with entropy ===================================
 entropy <- function(x, base = 2) { # calculate entropy
     X <- outer(x, unique(x), "==")
     X <- apply(X, 2, mean)
@@ -10,19 +8,20 @@ entropy <- function(x, base = 2) { # calculate entropy
 }
 
 
-child_gain <- function(parent, child, ig = FALSE){
+information_gain <- function(parent, child, score = FALSE){
     child_labels <- unique(child)
     K <- length(child_labels)
     child_per <- apply(outer(child, child_labels, "=="), 2, mean)
     gain <- vector("numeric", K)
     for (i in seq(K)) { gain[i] <- entropy(parent[child == child_labels[i]]) }
-    if(ig){
+    if (score) {
         return(as.numeric(entropy(parent) - child_per %*% gain))
     }
     cbind.data.frame(label = child_labels, per = child_per, entropy = gain)
 }
 
 
+# Prep data
 mushrooms <- read.table(
     "/Users/dag/github/learning/ds4Business/agaricus-lepiota.data",
     header = FALSE, sep = ",",
@@ -35,20 +34,24 @@ mushrooms <- read.table(
     ),
     stringsAsFactors = FALSE
 )
-mushrooms[, "propensity"] <- runif(nrow(mushrooms), 0, 1)
+set.seed(01012016)
 train <- sample(seq(nrow(mushrooms)), 5644, replace = FALSE) # N_obs stated
-mushrooms_train <- mushrooms[train, -ncol(mushrooms_train)]
+mushrooms_train <- mushrooms[train, ]
 
+# Test run feature selection
 entropy(mushrooms_train$edible) # in text: .96 (p. 59)
-pdta <- child_gain(mushrooms_train$edible, mushrooms_train$odor)
-ggplot(data = pdta, aes(x = reorder(label, entropy), y = entropy, fill = per)) +
+pdta <- information_gain(mushrooms_train$edible, mushrooms_train$odor)
+ggplot( # entropy diagram
+    data = pdta, aes(x = reorder(label, entropy), y = entropy, fill = per)
+) +
     geom_bar(stat = "identity")
 pdta[, 2] %*% pdta[, 3] # entropy in edible left after knowing odor
 entropy(mushrooms_train$edible) - pdta[, 2] %*% pdta[, 3] # information gain
 
+# Calculate information gain for each candidate feature.
 igs <- apply(mushrooms_train[, -1], 2,
     function(x){
-        child_gain(parent = mushrooms_train$edible, child = x, ig = TRUE)
+        information_gain(parent = mushrooms_train$edible, child = x, score = TRUE)
     }
 )
 sort(igs)
