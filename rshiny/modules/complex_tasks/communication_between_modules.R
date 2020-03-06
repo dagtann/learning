16a1fdb7121a2bc5dcb48de462407aebec769d12
Dag Tanneberg <dag.tanneberg@googlemail.com>
2020-03-06 08:12:45 +0100

    Add simple return value scenario

--- rshiny/modules/complex_tasks/communication_between_modules.R
+++ rshiny/modules/complex_tasks/communication_between_modules.R
@@ -1,6 +1,7 @@
 rm(list = ls())
 
 library(shiny)
+library(shinyWidgets)
 library(ggplot2)
 
 # Inner module setup ==========================================================
@@ -8,24 +9,27 @@ inner_module_ui <- function(id) {
   ns <- NS(id)
   
   tagList(
-    plotOutput(ns("inner_plot"))
+    plotOutput(ns("inner_plot")),
+    materialSwitch(ns("export_switch"))
   )
 }
 
 inner_module <- function(input, output, session) {
   # preallocate object
   string_return <- character(1)
-
+  
+  plot_data <- data.frame(x = runif(10), y = runif(10))
   plot_object <- reactive({
-    data <- data.frame(x = runif(10), y = runif(10))
-    ggplot(data = data, aes(x = x, y = y)) +
+    ggplot(data = plot_data, aes(x = x, y = y)) +
       geom_point() +
       labs(title = "I am inside the inner module")
   })
-  
   output$inner_plot <- renderPlot({plot_object()})
   
-  return(isolate(plot_object()))
+  return_value <- eventReactive(input$export_switch, {
+    input$export_switch
+  })
+  return(reactive(return_value()))
 }
 
 # Outer module setup ==========================================================
@@ -34,16 +38,15 @@ outer_module_ui <- function(id) {
   
   fluidPage(
     inner_module_ui(ns("plot")),
-    plotOutput(ns("text"))
+    verbatimTextOutput(ns("text"))
   )
 }
 
 outer_module <- function(input, output, session) {
   inner_plot_return <- callModule(inner_module, "plot")
 
-  output$text <- renderPlot({
-    inner_plot_return + geom_line(colour = "red") +
-      labs(title = "I am inside the outer module")
+  output$text <- renderText({
+    paste("I am reactive:", is.reactive(inner_plot_return()), "My value is:", inner_plot_return())
   })
 }
 
